a692cc361e5e5dce25f855f446215938554fa4da
Liju <ppml38@gmail.com>
2023-03-19 02:02:19 +0530

    added logic for collision detection

--- game.js
+++ game.js
@@ -1,15 +1,25 @@
 import {aircraft} from "./aircraft.js";
 import {bullet} from "./bullet.js";
 import {obstacle} from "./obstacle.js";
+class defaultdict {
+  constructor(defaultVal) {
+    return new Proxy({}, {
+      get: (target, name) => name in target ? target[name] : defaultVal
+    })
+  }
+}
 export class game{
     constructor(){
         // game view
         this.canvas = document.createElement("canvas");
-        this.canvas_height = 300;
-        this.canvas_width = 300;
+        this.canvas_height = 600;
+        this.canvas_width = 1200;
         this.canvas.height = this.canvas_height;
         this.canvas.width = this.canvas_width;
         this.ctx = this.canvas.getContext("2d");
+        this.launch_sound = new Audio("./launch.wav");
+        this.launch_sound.preload = 'auto';
+        this.launch_sound.load();
 
 
         // game states and constants
@@ -17,11 +27,12 @@ export class game{
         this.down_key_pressed = false;
         this.space_key_pressed = false;
         this.canvas_refresh_rate = 10; // one frame for 10ms i.e 100 frames/second
-        this.obstacle_rate = 2000; // new obstacle will be added every 2 seconds.
+        this.obstacle_rate = 1000; // new obstacle will be added every 2 seconds.
         this.aircraft = new aircraft();
         this.bullets=[];
         this.obstacles=[];
         this.previous_second = 0;
+        this.crossoverMap=new defaultdict([]);// map that tracks bullets with obstacle on same y axis path {bullet.y: obstacle}
 
         // adding event listeners for user input
         document.onkeydown = (event)=>{
@@ -33,7 +44,11 @@ export class game{
                 this.down_key_pressed = true;
             }
             else if (e.keyCode == '32') {
-                this.shoot();
+                if(this.space_key_pressed===false){
+                    this.space_key_pressed = true;
+                    this.shoot();
+                    this.space_key_pressed = false;
+                }
             }
         }
         document.onkeyup = (event)=>{
@@ -47,11 +62,9 @@ export class game{
         }
 
     }
-    shoot(){
-        this.bullets.push(new bullet(this));
-    }
-    getBackground(){
-
+    playLaunchSound() {
+      var click=this.launch_sound.cloneNode();
+      click.play();
     }
     run(){
         /*
@@ -66,25 +79,37 @@ export class game{
         // set spacecraft position
         this.aircraft.render(this);
 
-        // add a new obstacle like meteor/missile
-        /*let current_second = new Date().getSeconds();
-        if(Math.abs(current_second-this.previous_second)>2 && Math.random() < 0.5){
-            this.obstacles.push(new obstacle(this));
-            this.previous_second = current_second;
-        }*/
-
         // set current position for all bullets and obstacles
-        this.obstacles = this.obstacles.filter((obstacle)=>{
-            return obstacle.render(this);
-        });
         this.bullets = this.bullets.filter((bullet)=>{
             return bullet.render(this);
+        });
+        this.obstacles = this.obstacles.filter((obstacle)=>{
+            return obstacle.render(this);
         });
 
         // if game over. exit.
     }
+    shoot(){
+        let new_bullet = new bullet(this);
+        this.obstacles.map((obs)=>{
+            if(new_bullet.onThePath(obs)===true){
+                this.crossoverMap[new_bullet.y].push(obs);
+                //console.log("added to map");
+                //console.log(this.crossoverMap[new_bullet.y])
+            }
+        });
+        this.bullets.push(new_bullet);
+        //this.playLaunchSound();
+        //console.log(this.crossoverMap);
+    }
     addObstacle(){
-        this.obstacles.push(new obstacle(this));
+        let new_obs = new obstacle(this);
+        this.bullets.map((bullet)=>{
+            if(bullet.onThePath(new_obs)){
+                this.crossoverMap[bullet.y].push(new_obs);
+            }
+        });
+        this.obstacles.push(new_obs);
     }
     render(){
         // starting the game
